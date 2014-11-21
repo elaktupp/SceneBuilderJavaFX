69ee61991cae633c5d06691ed8ec4060cf803f57
elaktupp <elaktupp@@users.noreply.github.com>
2014-11-21 16:02:01 +0200

    Just scene no functionality

--- src/scenebuilderjavafx/FXMLDocumentController.java
+++ src/scenebuilderjavafx/FXMLDocumentController.java
@@ -8,11 +8,19 @@ package scenebuilderjavafx;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -26,6 +34,9 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     Label underLabel;
+
+    @FXML
+    ListView operationsList;
     
     @FXML
     ColorPicker colorSelector;
@@ -45,11 +56,22 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleColorSelection(ActionEvent e) {
-        underLabel.setText(colorSelector.getValue().toString());
+        char[] temp = colorSelector.getValue().toString().toCharArray();
+        char[] color = new char[(temp.length-1)];
+        
+        color[0] = '#';
+        
+        for (int i=2; i<temp.length; i++) {
+            color[i-1] = temp[i];
+        }
+        
+        String hashHexColor = String.valueOf(color);
+        
+        root.setStyle("-fx-background: "+hashHexColor);
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
 }
